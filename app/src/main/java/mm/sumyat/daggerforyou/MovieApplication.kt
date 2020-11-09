d900777dd370ae525cd69ac126f43bb2c33210c4
Aung Ye Htet <agye.tech@gmail.com>
2020-11-09 13:40:45 +0630

    refactor: MultiDex

--- app/src/main/java/mm/sumyat/daggerforyou/MovieApplication.kt
+++ app/src/main/java/mm/sumyat/daggerforyou/MovieApplication.kt
@@ -2,6 +2,7 @@ package mm.sumyat.daggerforyou
 
 import android.app.Activity
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -11,7 +12,7 @@ import timber.log.Timber
 import javax.inject.Inject
 
 @InternalCoroutinesApi
-class MovieApplication : Application(), HasAndroidInjector {
+class MovieApplication : MultiDexApplication(), HasAndroidInjector {
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
